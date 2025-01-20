8788a2f2137ba741393f22c29e433bf0e01a8d00
moucmoi <noatoimoi@gmail.com>
2025-01-20 18:20:49 +0100

    ajout page connexion

---
+++ src/views/pageConnexion.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="">
+    <title>Connexion</title>
+</head>
+
+<body>
+    <div class="header">
+        <a href="http://www.undouadejazz.com">Retour</a>
+        <h1>Se connecter</h1>
+    </div>
+    <div class="content">
+
+        <form class="connexion" method="POST" action="vérificationConnexion">
+            <input type="text" name="ident" id="ident" placeholder="Identifiant" />
+            <input type="password" name="passwd" id="passwd" placeholder="Mot de passe" />
+            <button type="button" name="toggle-password" id="toggle-password" class="toggle-password"
+                data-target="passwd">afficher</button>
+            <?php
+            if (!empty($_SESSION['connexion_fail'])) {
+                $_SESSION['connexion_fail'] = false;
+                echo "<p class='fail'>L'identifiant ou le mot de passe est incorrect</p>";
+            } ?>
+            <input class="btn-connexion" type="submit" value="Se connecter" />
+        </form>
+    </div>
+
+    <script>
+        document.querySelectorAll('.toggle-password').forEach(button => {
+            button.addEventListener('click', () => {
+                const targetId = button.getAttribute('data-target');
+                const passwordField = document.getElementById(targetId);
+                if (passwordField.type === 'password') {
+                    passwordField.type = 'text';
+                    button.textContent = 'Cacher';
+                } else {
+                    passwordField.type = 'password';
+                    button.textContent = 'Afficher';
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
