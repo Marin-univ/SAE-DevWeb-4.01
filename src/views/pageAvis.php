f6e4c3226772bc94f8b1aac1f88e153582e18c27
marin <marin.chesneau31@gmail.com>
2025-03-06 16:46:56 +0100

    test

---
+++ src/views/pageAvis.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html lang='fr'>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Vos Avis</title>
+        <link rel="stylesheet" href="../../public/assets/css/avis.css">
+    </head>
+    <body>
+        <?php
+            use src\controllers\Database;
+            $bdd = Database::getMysqlConnection();
+
+            $mail = $_SESSION["mail"];
+            $idU = $_SESSION["id"];
+
+            $Avis = $bdd->prepare('SELECT * FROM AVIS NATURAL JOIN RESTAURANT WHERE idU=:id');
+            $Avis->bindParam(":id", $idU, PDO::PARAM_STR);
+            $Avis->execute();
+            $lesAvis = $Avis->fetchAll();
+        ?>
+        <div class="header">
+            <a href="/">Retour</a>
+            <h1>Vos Avis</h1>
+        </div>
+        <?php foreach($lesAvis as $avis){?>
+            <div id=avis>
+                <p>Avis sur le restaurant : <?php echo htmlspecialchars($avis['nomR'])?></p>
+                <p><?php echo htmlspecialchars($avis['note'])?>/5</p>
+                <p><?php echo htmlspecialchars($avis['description'])?></p>
+            </div>
+        <?php }?>
+    </body>
+</html>
