c0d5e1a6294ab958a7a9ea14bfb43657d480b24e
marin <marin.chesneau31@gmail.com>
2025-03-07 08:22:18 +0100

    resolution bug

--- src/views/pageConnexion.php
+++ src/views/pageConnexion.php
@@ -16,8 +16,8 @@
 
         <form class="connexion" method="POST" action="verfiCo">
             <input type="email" name="mail" id="ident" placeholder="Mail" required/>
-            <input type="password" name="passwd" id="passwd" placeholder="Mot de passe" required/>
-            <button type="button" name="toggle-password" id="new-passwd" class="toggle-password">afficher</button>
+            <input type="password" name="passwd" id="new-passwd" placeholder="Mot de passe" required/>
+            <button type="button" name="toggle-password" id="toggle-all-passwords" class="toggle-password">afficher</button>
             <?php
             if (isset($_SESSION['connexion-fail'])) {
                 if ($_SESSION['connexion-fail'] === "true") {
