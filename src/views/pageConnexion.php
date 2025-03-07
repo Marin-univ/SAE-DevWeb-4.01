872dafbe85f1762ef50228259a33c934e19d6425
Marin Chesneau <144822096+Marin-univ@users.noreply.github.com>
2025-03-07 08:17:21 +0100

    Petit avancement (#16)
    * avancement à finir
    
    * fin du css

--- src/views/pageConnexion.php
+++ src/views/pageConnexion.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="../../public/assets/css/connexion-inscription.css">
     <title>Connexion</title>
 </head>
 
@@ -17,8 +17,7 @@
         <form class="connexion" method="POST" action="verfiCo">
             <input type="email" name="mail" id="ident" placeholder="Mail" required/>
             <input type="password" name="passwd" id="passwd" placeholder="Mot de passe" required/>
-            <button type="button" name="toggle-password" id="toggle-password" class="toggle-password"
-                data-target="passwd">afficher</button>
+            <button type="button" name="toggle-password" id="new-passwd" class="toggle-password">afficher</button>
             <?php
             if (isset($_SESSION['connexion-fail'])) {
                 if ($_SESSION['connexion-fail'] === "true") {
@@ -29,21 +28,6 @@
             <input class="btn-connexion" type="submit" value="Se connecter" />
         </form>
     </div>
-
-    <script>
-        document.querySelectorAll('.toggle-password').forEach(button => {
-            button.addEventListener('click', () => {
-                const targetId = button.getAttribute('data-target');
-                const passwordField = document.getElementById(targetId);
-                if (passwordField.type === 'password') {
-                    passwordField.type = 'text';
-                    button.textContent = 'Cacher';
-                } else {
-                    passwordField.type = 'password';
-                    button.textContent = 'Afficher';
-                }
-            });
-        });
-    </script>
+    <script src="../../public/assets/js/affiche_mdp.js"></script>
 </body>
 </html>
