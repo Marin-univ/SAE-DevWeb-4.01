9b349b64fbdd57312030524dc3dcb9668e90f833
moucmoi <noatoimoi@gmail.com>
2025-03-07 13:52:41 +0100

    pas d'erreur mais ça marche pas

--- src/controllers/Restaurant.php
+++ src/controllers/Restaurant.php
@@ -8,15 +8,22 @@ class Restaurant{
     public $phone;
     public $website;
 
+    public function __construct($type, $nom, $phone, $website) {
+        $this->type = $type;
+        $this->nom = $nom;
+        $this->phone = $phone;
+        $this->website = $website;
+    }
 
     public function affichage(){
         $chemin_image="/public/assets/images/" . $this->type;
         return <<<HTML
-            <section>
-                <img src="{$chemin_image}" alt="image resto">
+            <link rel="stylesheet" href="/public/assets/css/restaurantUnique.css">
+            <section class="section-détail-resto">
+                <img id="image-resto" src="{$chemin_image}" alt="image resto">
             </section>
-            <section>
-                <h3>$this->nom</h3>
+            <section class="section-détail-resto">>
+                <h3 id="texte-resto">$this->nom</h3>
             </section>
         HTML;
     }
--- src/views/acceuil.php
+++ src/views/acceuil.php
@@ -10,7 +10,7 @@ use src\controllers\Map;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
-    <link rel="stylesheet" href="../../public/assets/css/pageAcceuil.css">
+    <link rel="stylesheet" href="/public/assets/css/pageAcceuil.css">
 </head>
 <body>
 
